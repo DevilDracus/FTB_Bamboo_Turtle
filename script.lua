883b3982ec225eab1085c998480482d6d71f4b30
Florian Moldenhauer <devildracus@my.com>
2023-11-12 15:31:43 +0100

    Update script.lua

--- script.lua
+++ script.lua
@@ -2,7 +2,7 @@
 branch = {amount = 1, 	-- the amount of "branch-pairs"
 	  length = 16,	-- the length of each branch
 	  space  = 5,
-	  height = 1}	-- the space between each branch-pair
+	  height = 8}	-- the space between each branch-pair
 slot = {fuel  = 1,	-- the slotnumber for fuel
 	torch = 2,	-- the slotnumber for torches
 	fill  = 3}	-- the slotnumber for filling material
@@ -13,12 +13,6 @@ other = {torch = true,  -- place torches? (true=yes/false=no)
 
 function main()
 	while true do
-		if turtle.getItemCount(1) > 0 then
-			other.hasFuel = true
-		else
-		other.hasFuel = false
-		end
-	while other.hasFuel do
 		refuel(1+(branch.height*branch.length*4)/96)
 		up(branch.height)
 		for i=1, branch.height, 1 do		  
@@ -31,7 +25,6 @@ function main()
 		end
 		dropLoot()
 		 sleep(10)
-	end
 	end
 end
 
@@ -48,8 +41,6 @@ function forward(length)
    sleep(0.5)
   end
   turnLeft()
-  sleep(2)
-  turtle.suckDown()
   turtle.forward()
  end
 end
@@ -58,7 +49,7 @@ function refuel(amount)
  if turtle.getFuelLevel() == "unlimited" then return end
  if turtle.getFuelLevel() < 96*amount then
   turtle.select(slot.fuel)
-  turtle.refuel(amount)
+  turtle.refuel()
  end
 end
 function back(length)
