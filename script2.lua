9da8583a775daa8d52450034a0614f1da166975d
Florian Moldenhauer <devildracus@my.com>
2023-11-12 12:59:41 +0100

    Update script2.lua

--- script2.lua
+++ script2.lua
@@ -2,30 +2,36 @@
 branch = {amount = 1, 	-- the amount of "branch-pairs"
 	  length = 16,	-- the length of each branch
 	  space  = 5,
-	  height = 10}	-- the space between each branch-pair
+	  height = 7}	-- the space between each branch-pair
 slot = {fuel  = 1,	-- the slotnumber for fuel
 	torch = 2,	-- the slotnumber for torches
 	fill  = 3}	-- the slotnumber for filling material
 other = {torch = true,  -- place torches? (true=yes/false=no)
-         close = false}  -- close the branches? (true=yes/false=no)
+         close = false,
+	hasFuel = false}  -- close the branches? (true=yes/false=no)
 -- END OF ADJUSTMENTS
 
 function main()
 	while true do
-		for i=1, branch.height, 1 do
-		 turtle.up()
+		if turtle.getItemCount(1) > 0 then
+			other.hasFuel = true
+		else
+		other.hasFuel = false
 		end
-		for i=1, branch.heigth, 1 do
-		 for i=1, branch.amount, 1 do
-		  refuel(1+(branch.space+branch.length*4)/96)
-		  turtle.up()
-		  forward(branch.length)
-		  turtle.down()
-		  back(branch.length)
-		 end
+	while other.hasFuel do
+		refuel(1+(branch.height*branch.length*4)/96)
+		up(branch.height)
+		for i=1, branch.height, 1 do		  
+		 forward(branch.length)
 		 turtle.down()
+		 back(branch.length)
 		 end
+		while turtle.detectDown() == false do
+		 turtle.down()
+		end
+		dropLoot()
 		 sleep(10)
+	end
 	end
 end
 
@@ -58,9 +64,6 @@ end
 
 function back(length)
  for i=1, length, 1 do
-  -- if i==9 then torch() end --places a torch after the first 8 blocks and
-  -- if (i-8)%16==0 and i>9 then torch() end --then another every 16 blocks  -- if i==9 then torch() end --places a torch after the first 8 blocks and
-  -- if (i-8)%16==0 and i>9 then torch() end --then another every 16 blocks
   turtle.back()
   if i==length-1 and other.close then --closes the branch 
    turtle.select(slot.fill)
@@ -82,11 +85,28 @@ function turnAround()
  turtle.turnRight()
 end
 
+function up(height)
+ for i=1, height, 1 do
+	turtle.up()
+end
+end
+
 function torch()
  if other.torch then
   turtle.select(slot.torch)
   turtle.place()
  end
+end
+
+function dropLoot()
+	turnAround()
+	if turtle.detect() then
+		for i=2, 16, 1 do
+			turtle.select(i)
+			turtle.drop()
+		end
+	end
+	turnAround()
 end
 
 main()
